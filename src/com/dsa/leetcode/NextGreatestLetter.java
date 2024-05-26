76b9ca32e1de7f3486d3c51683ba5ee9837eb849
neha_wasnik <neha.wasnik@ftcash.com>
2024-05-26 20:54:39 +0530

    next great letter

---
+++ src/com/dsa/leetcode/NextGreatestLetter.java
@@ -0,0 +1,25 @@
+package com.dsa.leetcode;
+
+public class NextGreatestLetter {
+    public static void main(String[] args) {
+        char[] letters = {'x','x','y','y'};
+        char target = 'z';
+        char ans = nextGreatestLetter(letters,target);
+        System.out.println(ans);
+    }
+    static char nextGreatestLetter(char[] letters, char target) {
+
+        int start = 0;
+        int end = letters.length - 1;
+        while(start <= end){
+
+            int mid = start + ((end - start)/2);
+            if (letters[mid]>target){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}

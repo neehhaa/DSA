636f3ceb77e068616a72d81adf3aa79d624bd705
neehhaa <nehawasnik95@outlook.com>
2024-06-19 01:25:20 +0530

    Sort Colors 0ms 0,1,2

---
+++ src/com/dsa/leetcode/SortColors.java
@@ -0,0 +1,35 @@
+package com.dsa.leetcode;
+
+public class SortColors {
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+
+
+    }
+
+    public static void sortColors(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+}
